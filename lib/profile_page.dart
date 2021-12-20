3a0d96148c70839d86bd2d9d4b02e85f04940492
Damla <damla.dalkilic@ogr.iuc.edu.tr>
2021-12-20 23:57:28 +0300

    profile page update

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -1,214 +1,225 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
 class ProfileUI2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
-          child: Column(
-
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                    image: DecorationImage(
-                        image: NetworkImage(
-                            "add you image URL here "
-                        ),
-                        fit: BoxFit.cover
-                    )
-                ),
-                child: Container(
-                  width: double.infinity,
-                  height: 200,
-                  child: Container(
-                    alignment: Alignment(0.0,2.5),
-                    child: CircleAvatar(
-                      backgroundImage: NetworkImage(
-                          "Add you profile DP image URL here "
-                      ),
-                      radius: 60.0,
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 60,
-              ),
-              Text(
-                "Damla"
-                ,style: TextStyle(
-                  fontSize: 25.0,
-                  color:Colors.blueGrey,
-                  letterSpacing: 2.0,
-                  fontWeight: FontWeight.w400
-              ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                "Istanbul, Türkiye"
-                ,style: TextStyle(
-                  fontSize: 18.0,
-                  color:Colors.black45,
-                  letterSpacing: 2.0,
-                  fontWeight: FontWeight.w300
-              ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                "'You can add a status'"
-                ,style: TextStyle(
-                  fontSize: 15.0,
-                  color:Colors.black45,
-                  letterSpacing: 2.0,
-                  fontWeight: FontWeight.w300
-              ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Card(
-                  margin: EdgeInsets.symmetric(horizontal: 20.0,vertical: 8.0),
-                  elevation: 2.0,
-                  child: Padding(
-                      padding: EdgeInsets.symmetric(vertical: 12,horizontal: 30),
-                      child: Text("Favourites",style: TextStyle(
-                          letterSpacing: 2.0,
-                          fontWeight: FontWeight.w300
-                      ),))
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              Text(
-                "The books they read"
-                ,style: TextStyle(
-                  fontSize: 18.0,
-                  color:Colors.black45,
-                  letterSpacing: 2.0,
-                  fontWeight: FontWeight.w300
-              ),
-              ),
-              Card(
-                margin: EdgeInsets.symmetric(horizontal: 20.0,vertical: 8.0),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Expanded(
-                        child: Column(
-                          children: [
-                            Text("Rated",
-                              style: TextStyle(
-                                  color: Color(0xff5ac18e),
-                                  fontSize: 22.0,
-                                  fontWeight: FontWeight.w600
-                              ),),
-                            SizedBox(
-                              height: 7,
-                            ),
-                            Text("150",
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 22.0,
-                                  fontWeight: FontWeight.w300
-                              ),)
-                          ],
-                        ),
-                      ),
-                      Expanded(
-                        child:
-                        Column(
-                          children: [
-                            Text("Commented",
-                              style: TextStyle(
-                                  color: Color(0xff5ac18e),
-                                  fontSize: 22.0,
-                                  fontWeight: FontWeight.w600
-                              ),),
-                            SizedBox(
-                              height: 7,
-                            ),
-                            Text("20",
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 22.0,
-                                  fontWeight: FontWeight.w300
-                              ),)
-                          ],
-                        ),
-                      ),
+      child: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                    tileMode: TileMode.mirror,
+                    stops: [
+                      0.0,
+                      0.4,
+                      0.6,
+                      1
                     ],
-                  ),
+                    colors: [
+                      Color(0x995ac18e),
+                      Color(0x665ac18e),
+                      Color(0x665ac18e),
+                      Color(0x995ac18e),
+                    ]),
+                image: DecorationImage(
+                    image: NetworkImage("add your image URL here "),
+                    fit: BoxFit.cover)),
+            child: Container(
+              width: double.infinity,
+              height: 200,
+              child: Container(
+                alignment: Alignment(0.0, 2.5),
+                child: CircleAvatar(
+                  backgroundImage:
+                      NetworkImage("https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png"),
+                  radius: 60.0,
                 ),
               ),
-              SizedBox(
-                height: 50,
-              ),
-              Row(
+            ),
+          ),
+          SizedBox(
+            height: 60,
+          ),
+          Text(
+            "Damla",
+            style: TextStyle(
+                fontSize: 25.0,
+                color: Colors.blueGrey,
+                letterSpacing: 2.0,
+                fontWeight: FontWeight.w400),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            "Istanbul, Türkiye",
+            style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.black45,
+                letterSpacing: 2.0,
+                fontWeight: FontWeight.w300),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            "'You can add a status'",
+            style: TextStyle(
+                fontSize: 15.0,
+                color: Colors.black45,
+                letterSpacing: 2.0,
+                fontWeight: FontWeight.w300),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Card(
+              margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
+              elevation: 2.0,
+              child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: 12, horizontal: 30),
+                  child: Text(
+                    "Favourites",
+                    style: TextStyle(
+                        letterSpacing: 2.0, fontWeight: FontWeight.w300),
+                  ))),
+          SizedBox(
+            height: 15,
+          ),
+          Text(
+            "The books they read",
+            style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.black45,
+                letterSpacing: 2.0,
+                fontWeight: FontWeight.w300),
+          ),
+          Card(
+            margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  RaisedButton(
-                    onPressed: (){
-                    },
-                    shape:  RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(80.0),
-                    ),
-                    child: Ink(
-                      decoration: BoxDecoration(
-                        color: Color(0xff5ac18e),
-                        borderRadius: BorderRadius.circular(30.0),
-                      ),
-                      child: Container(
-                        constraints: BoxConstraints(maxWidth: 100.0,maxHeight: 40.0,),
-                        alignment: Alignment.center,
-                        child: Text(
-                          "See Books",
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Text(
+                          "Rated",
                           style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 12.0,
-                              letterSpacing: 2.0,
-                              fontWeight: FontWeight.w300
-                          ),
+                              color: Color(0xff5ac18e),
+                              fontSize: 22.0,
+                              fontWeight: FontWeight.w600),
                         ),
-                      ),
+                        SizedBox(
+                          height: 7,
+                        ),
+                        Text(
+                          "150",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 22.0,
+                              fontWeight: FontWeight.w300),
+                        )
+                      ],
                     ),
                   ),
-                  RaisedButton(
-                    onPressed: (){
-                    },
-                    shape:  RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(80.0),
-                    ),
-                    child: Ink(
-                      decoration: BoxDecoration(
-                        color: Color(0xff5ac18e),
-                        borderRadius: BorderRadius.circular(80.0),
-                      ),
-                      child: Container(
-                        constraints: BoxConstraints(maxWidth: 100.0,maxHeight: 40.0,),
-                        alignment: Alignment.center,
-                        child: Text(
-                          "Send DM",
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Text(
+                          "Commented",
                           style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 12.0,
-                              letterSpacing: 2.0,
-                              fontWeight: FontWeight.w300
-                          ),
+                              color: Color(0xff5ac18e),
+                              fontSize: 22.0,
+                              fontWeight: FontWeight.w600),
                         ),
-                      ),
+                        SizedBox(
+                          height: 7,
+                        ),
+                        Text(
+                          "20",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 22.0,
+                              fontWeight: FontWeight.w300),
+                        )
+                      ],
                     ),
-                  )
+                  ),
                 ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              RaisedButton(
+                onPressed: () {},
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(80.0),
+                ),
+                child: Ink(
+                  decoration: BoxDecoration(
+                    color: Color(0xff5ac18e),
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                  child: Container(
+                    constraints: BoxConstraints(
+                      maxWidth: 100.0,
+                      maxHeight: 40.0,
+                    ),
+                    alignment: Alignment.center,
+                    child: Text(
+                      "See Books",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 12.0,
+                          letterSpacing: 2.0,
+                          fontWeight: FontWeight.w300),
+                    ),
+                  ),
+                ),
+              ),
+              RaisedButton(
+                onPressed: () {},
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(80.0),
+                ),
+                child: Ink(
+                  decoration: BoxDecoration(
+                    color: Color(0xff5ac18e),
+                    borderRadius: BorderRadius.circular(80.0),
+                  ),
+                  child: Container(
+                    constraints: BoxConstraints(
+                      maxWidth: 100.0,
+                      maxHeight: 40.0,
+                    ),
+                    alignment: Alignment.center,
+                    child: Text(
+                      "Send DM",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 12.0,
+                          letterSpacing: 2.0,
+                          fontWeight: FontWeight.w300),
+                    ),
+                  ),
+                ),
               )
             ],
-          ),
-        )
-    );
+          )
+        ],
+      ),
+    ));
   }
 }
